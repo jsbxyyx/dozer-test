dda05026ac867ca379a6fc3a0d0ec2ad08968356
jsbxyyx <jsbxyyx@163.com>
2018-04-03 19:41:31 +0800

    Create A.java

---
+++ src/test/java/org/jsbxyyx/test/A.java
@@ -0,0 +1,33 @@
+package org.jsbxyyx.test;
+
+/**
+ * @author jsbxyyx
+ * 
+ */
+public class A {
+
+	private String date;
+	private String vNum;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getVNum() {
+		return vNum;
+	}
+
+	public void setVNum(String vNum) {
+		this.vNum = vNum;
+	}
+
+	@Override
+	public String toString() {
+		return "A [date=" + date + ", vNum=" + vNum + "]";
+	}
+
+}
